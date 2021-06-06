a17e7ce6835145cde03504df4de720dc1a80d8c4
Pranay Gupta <pranayguptamain@gmail.com>
2021-06-06 19:43:46 +0530

    Update 09 object2.js

--- Hitesh Choudhary/03 Intermediate/09 object2.js
+++ Hitesh Choudhary/03 Intermediate/09 object2.js
@@ -4,4 +4,19 @@ var user = {
     role: "Procrastinator",
     loginCount: 69,
     facebookSignedIn: true,
+    courseList: [],
+    buyCourse: function (courseName) {
+        this.courseList.push(courseName);
+    },
+    getCourseCount: function () {
+        return `${this.firstName} is enrolled in total of ${this.courseList.length} courses`;
+    },
 };
+
+// var courseList = true;
+// console.log(user.firstName);
+console.log(user.getCourseCount());
+user.buyCourse("Complete JS course");
+user.buyCourse("React JS course");
+user.buyCourse("Angular course");
+console.log(user.getCourseCount());
