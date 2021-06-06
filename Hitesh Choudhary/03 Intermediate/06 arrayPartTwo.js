2d59011ced9a14f99a3838b7a3037580b25f5d2c
Pranay Gupta <pranayguptamain@gmail.com>
2021-06-06 18:51:27 +0530

    Update 06 arrayPartTwo.js

--- Hitesh Choudhary/03 Intermediate/06 arrayPartTwo.js
+++ Hitesh Choudhary/03 Intermediate/06 arrayPartTwo.js
@@ -9,4 +9,8 @@ var isEven = (element) => {
 
 
 var result = [2, 4, 6, 8].every(isEven); // every runs the operation through every element in the array
+// also note that the value of isEven is passed by reference
+console.log(result);
+
+var result = [2, 3, 6, 8].every((e) => (e % 2 === 0));
 console.log(result);
