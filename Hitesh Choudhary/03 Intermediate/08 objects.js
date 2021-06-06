6139a3ac4de1124325b83a80b85ed3735aad2057
Pranay Gupta <pranayguptamain@gmail.com>
2021-06-06 19:30:49 +0530

    Create 08 objects.js

---
+++ Hitesh Choudhary/03 Intermediate/08 objects.js
@@ -0,0 +1,10 @@
+var user = {
+    firstName: "Pranay",
+    lastName: "Gupta",
+    role: "Procrastinator",
+    loginCount: 69,
+    facebookSignedIn: true,
+};
+
+console.log(user.firstName);
+console.log(user["lastName"]); // boomer way
