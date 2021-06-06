39671b9290dd163aeb3c073211b79eb9d80c5b57
Pranay Gupta <pranayguptamain@gmail.com>
2021-06-06 20:45:55 +0530

    while loop

--- Hitesh Choudhary/03 Intermediate/10 loops.js
+++ Hitesh Choudhary/03 Intermediate/10 loops.js
@@ -7,7 +7,13 @@ const myStates = [
     "Maharashtra",
 ];
 
-for (let i = 0; i < myStates.length; i++) {
-    if (typeof myStates[i] !== "string") break;
+// for (let i = 0; i < myStates.length; i++) {
+//     if (typeof myStates[i] !== "string") break;
+//     console.log(myStates[i]);
+// }
+
+let i = 0;
+while (i < myStates.length) {
     console.log(myStates[i]);
+    i++;
 }
